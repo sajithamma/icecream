ba10a6a9582062265f0661e54fb8c69b22fd9a94
Sajith Amma <mr.sajith@gmail.com>
2025-01-01 16:55:41 +0530

    price updated

--- server/payment_routes.py
+++ server/payment_routes.py
@@ -62,7 +62,7 @@ def create_order():
     transaction_id = str(uuid4())
 
     # Define amount and currency
-    amount = 700  # Amount in smallest currency unit (₹8.00 = 800 paisa)
+    amount = 86  # Amount in smallest currency unit (₹8.00 = 800 paisa)
     currency = "INR"
 
     # Log the payment in the database with PENDING status
